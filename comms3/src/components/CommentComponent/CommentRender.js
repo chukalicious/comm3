ecf43e3592bdea86ddb4d2691f79e696abe3dc28
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-09-04 23:26:31 -0700

    added scoring functionality to the plus and minus buttons

--- comms3/src/components/CommentComponent/CommentRender.js
+++ comms3/src/components/CommentComponent/CommentRender.js
@@ -1,19 +1,44 @@
+import { useState } from "react";
+
 const CommentRender = (props) => {
   console.log("ComentRender: props: ", props);
+
+  //   must have local state for the counter:
+  const [score, setScore] = useState(0);
+  console.log("CommentRender: score: ", score);
+
+  const handleUpVote = (e) => {
+    e.stopPropagation();
+    setScore(() => {
+      return score + 1;
+    });
+  };
+
+  const handleDownVote = (e) => {
+    e.stopPropagation();
+    setScore(() => {
+      return score - 1;
+    });
+  };
+  /////////////// END OF COUNTER
+
   return (
     <div>
       <div>
-        <img alt="user avatar" /> <h1>username</h1>
-        <h2>time ago</h2>
+        <img alt="user avatar" src={props.commentsData.user.image} />{" "}
+        <h1>{props.commentsData.user.username}</h1>
+        <h2>{props.commentsData.createdAt}</h2>
       </div>
       <div>
-        <p>comment</p>
+        <p>{props.commentsData.content}</p>
       </div>
       <div>
         <div>
-          <button>plus</button>the count number<button>minus</button>
+          <button onClick={handleUpVote}>plus</button>
+          {score}
+          <button onClick={handleDownVote}>minus</button>
         </div>
-        <button>reply</button>
+        <button>Reply</button>
       </div>
     </div>
   );
