cd2fc4a169b213b4c79afe95fda7ef49711488c7
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-09-04 22:14:20 -0700

    set up the html structure

--- comms3/src/components/CommentComponent/CommentRender.js
+++ comms3/src/components/CommentComponent/CommentRender.js
@@ -1,5 +1,21 @@
 const CommentRender = () => {
-  return <>render</>;
+  return (
+    <div>
+      <div>
+        <img alt="user avatar" /> <h1>username</h1>
+        <h2>time ago</h2>
+      </div>
+      <div>
+        <p>comment</p>
+      </div>
+      <div>
+        <div>
+          <button>plus</button>the count number<button>minus</button>
+        </div>
+        <button>reply</button>
+      </div>
+    </div>
+  );
 };
 
 export default CommentRender;
